f931e9de4647fd4cbd44a39f0b41ff9ce226e6ad
Mina Mansour <mansourmina10@gmail.com>
2021-09-22 21:54:53 +0200

    Aufgabe 4 verbessert

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,7 +28,8 @@
       </div>
     </v-main> -->
 
-    <v-app-bar class="blue" app>
+    <!-- Aufgabe 3 -->
+    <!-- <v-app-bar class="blue" app>
       <v-app-bar-nav-icon
         @click.stop="drawer = !drawer"
         class="white--text"
@@ -72,8 +73,98 @@
           {{ route.name }}
         </v-list-item>
       </v-list-item-group>
+    </v-navigation-drawer> -->
+
+    <!-- Aufgabe 4 -->
+    <v-app-bar class="blue" app>
+      <img
+        class="mx-2"
+        src="@/assets/tesla-motors-1.svg"
+        style="height:150px; margin-top:100px"
+      />
+      <span
+        class="text-truncate text-h4 blue-grey--text darken-1"
+        style="font-family: Poppins"
+        >Tesla Inc.</span
+      >
+      <v-spacer></v-spacer>
+      <div class="hidden-md-and-down">
+        <v-btn
+          active-class="white--text orange"
+          class="white--text black"
+          to="/"
+          exact
+          >Home</v-btn
+        >
+        <v-btn
+          active-class="white--text orange"
+          class="white--text black ml-5"
+          to="/products"
+          
+          >Products</v-btn
+        >
+        <v-btn
+          active-class="white--text orange"
+          class="white--text black ml-5"
+          to="/about"
+          exact
+          >About</v-btn
+        >
+      </div>
+
+      <v-app-bar-nav-icon
+        v-if="!drawer"
+        @click.stop="drawer = !drawer"
+        class="white--text hidden-lg-and-up"
+      ></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      app
+      hide-overlay
+      disable-resize-watcher
+      v-model="drawer"
+      right
+    >
+      <v-row>
+        <v-col cols="6">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Menu
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Please select
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col cols="6">
+          <v-btn text @click="drawer = false" float="right" class="mt-4"
+            >Close</v-btn
+          >
+        </v-col>
+      </v-row>
+
+      <v-divider></v-divider>
+      <v-list-item-group active-class="blue white--text">
+        <v-list-item
+          link
+          v-for="(route, i) in routes"
+          :key="i"
+          :to="route.link"
+        >
+          {{ route.name }}
+        </v-list-item>
+      </v-list-item-group>
     </v-navigation-drawer>
-    <v-main class="yellow"> </v-main>
+
+    <v-main>
+      <div class="d-flex">
+        <div style="width:250px"></div>
+        <div class="mt-10" style="flex:1"><router-view></router-view></div>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
@@ -91,6 +182,19 @@ export default {
 
     drawer: false,
   }),
+  methods: {
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent,
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 </script>
 
