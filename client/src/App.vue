58ec2ac1ea88315d7f250b414d3b1fc2696ef3d4
Mina Mansour <mansourmina10@gmail.com>
2021-09-22 14:03:13 +0200

    Aufgabe 3 erledigt

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar class="grey lighten-5" height="74" app>
+    <!-- Aufgabe 2 -->
+    <!-- <v-app-bar class="grey lighten-5" height="74" app>
       <img
         class="mx-2"
         src="@/assets/tesla-motors-1.svg"
@@ -25,7 +26,54 @@
         <div style="width:250px"></div>
         <div class="mt-10" style="flex:1"><router-view></router-view></div>
       </div>
-    </v-main>
+    </v-main> -->
+
+    <v-app-bar class="blue" app>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="white--text"
+      ></v-app-bar-nav-icon>
+      <span class="white--text title">Layout Demo</span>
+    </v-app-bar>
+    <v-navigation-drawer
+      app
+      hide-overlay
+      disable-resize-watcher
+      v-model="drawer"
+    >
+      <v-row>
+        <v-col cols="6">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Menu
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Please select
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col cols="6">
+          <v-btn text @click="drawer = false" float="right" class="mt-4"
+            >Close</v-btn
+          >
+        </v-col>
+      </v-row>
+
+      <v-divider></v-divider>
+      <v-list-item-group active-class="blue white--text">
+        <v-list-item
+          link
+          v-for="(route, i) in routes"
+          :key="i"
+          :to="route.link"
+        >
+          {{ route.name }}
+        </v-list-item>
+      </v-list-item-group>
+    </v-navigation-drawer>
+    <v-main class="yellow"> </v-main>
   </v-app>
 </template>
 
@@ -34,7 +82,14 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    routes: [
+      { link: '/', name: 'Home' },
+      { link: '/products', name: 'Products' },
+      { link: '/team', name: 'Team' },
+      { link: '/about', name: 'About' },
+    ],
+
+    drawer: false,
   }),
 };
 </script>
